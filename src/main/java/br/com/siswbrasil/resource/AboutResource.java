7ffda63625bd6fbc80398aacb9a033b3413b17f6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2024-10-16 20:29:17 -0300

    Refactor application.properties and AboutResource.java
    - Refactor application.properties to add project name and version properties.
    - Refactor AboutResource.java to inject project name, version, and Quarkus profile properties.

--- src/main/java/br/com/siswbrasil/resource/AboutResource.java
+++ src/main/java/br/com/siswbrasil/resource/AboutResource.java
@@ -2,6 +2,7 @@ package br.com.siswbrasil.resource;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -14,9 +15,21 @@ public class AboutResource {
     @Inject
     Template about;
 
+    @ConfigProperty(name = "project.version")
+    String projectVersion;
+
+    @ConfigProperty(name = "project.name")
+    String projectName;    
+
+    @Inject
+    @ConfigProperty(name = "quarkus.profile",defaultValue = "default")
+    String quarkusProfile;
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get() {
-        return about.data("quarkusProfile", System.getProperty("quarkus.profile"));
+        return about.data("quarkusProfile", quarkusProfile)
+                    .data("projectName", projectName)
+                    .data("projectVersion", projectVersion);
     }
 }
